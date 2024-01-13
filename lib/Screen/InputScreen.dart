b95dde9a1aa709b042a022ea612d936b304928b5
Anurag <dubeyjianurag@outlook.com>
2024-01-13 22:55:24 +0530

    basic

--- lib/Screen/InputScreen.dart
+++ lib/Screen/InputScreen.dart
@@ -1,4 +1,3 @@
-import 'package:bmi_calculator/Screen/ResultScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../widget/CustomMeasurementWidget.dart';
@@ -12,6 +11,8 @@ class InputScreenState extends State<InputScreen> {
   int heightValue = 150;
   int ageValue = 25;
   int weightValue = 70;
+  double bmiResult = 0.0;
+  String bmiCategory = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,12 +24,12 @@ class InputScreenState extends State<InputScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            buildListWheelScrollView(),
-            SizedBox(height: 10),
             Text(
               'Height: $heightValue cm',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
             ),
+            buildListWheelScrollView(),
+            SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -54,31 +55,90 @@ class InputScreenState extends State<InputScreen> {
                 )
               ],
             ),
-            ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> ResultScreen()));
-            }, child: Text('Submit'))
+            SizedBox(height: 10),
+            Container(
+              width: 400,
+              child: ElevatedButton(onPressed: (){
+                // Navigator.push(context, MaterialPageRoute(builder: (context)=> ResultScreen()));
+                calculatebmi();
+              }, style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states){
+                      if(states.contains(MaterialState.pressed)){
+                        return Colors.grey;
+                      }
+                      if(states.contains(MaterialState.hovered)){
+                        return Colors.cyanAccent;
+                      }
+                      return Colors.lightBlue;
+                    }
+                )
+              ),
+                child: Text('Submit',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.bold),),
+              )
+            ),
+            SizedBox(height: 16),
+            Text('BMI Result: ${bmiResult.toStringAsFixed(2)}',style: TextStyle(fontSize: 24),),
+            Text('Category: $bmiCategory'),
           ],
         ),
       ),
     );
   }
-
+  void calculatebmi(){
+    if(heightValue>0 && weightValue>0 && ageValue>0){
+      setState(() {
+        double heightInMeters= heightValue/100.0;
+        bmiResult=weightValue/(heightInMeters*heightInMeters);
+        bmiCategory=getbmicategory(bmiResult,ageValue);
+      });
+    }
+    else {
+      setState(() {
+        bmiResult = 0.0;
+        bmiCategory = '';
+      });
+    }
+  }
+  String getbmicategory(double bmi,int age){
+    if (age < 18) {
+      return 'BMI categories for children may differ';
+    }
+    if (bmi < 18.5) {
+      return 'Underweight';
+    } else if (bmi < 25) {
+      return 'Normal weight';
+    } else if (bmi < 30) {
+      return 'Overweight';
+    } else if (bmi < 35) {
+      return 'Obesity I';
+    } else if (bmi < 40) {
+      return 'Obesity II';
+    } else {
+      return 'Obesity III';
+    }
+  }
   Widget buildListWheelScrollView() {
     return Container(
       height: 200,
-      child: ListWheelScrollView(
+      child: ListWheelScrollView.useDelegate(
         itemExtent: 70,
         diameterRatio: 1.5,
         offAxisFraction: 0.5,
         physics: FixedExtentScrollPhysics(),
-        children: List.generate(
-          251,
-              (index) => Center(
-            child: Text(
-              '$index cm',
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
+        childDelegate: ListWheelChildBuilderDelegate(
+          builder: (BuildContext context, int index) {
+            return Center(
+              child: Text(
+                '$index cm',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: index == heightValue ? Colors.red : Colors.black,
+                ),
+              ),
+            );
+          },
+          childCount: 251,
         ),
         onSelectedItemChanged: (index) {
           setState(() {
@@ -88,4 +148,5 @@ class InputScreenState extends State<InputScreen> {
       ),
     );
   }
+
 }
